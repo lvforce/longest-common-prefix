d9d10e4fa2c5d5b5eca0f3910329245686198a3b
rp_prodan <one.prodan1@gmail.com>
2020-03-07 23:14:21 +0200

    cleaned the code

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
-class Solution:
-	"""The algorithm iterates over letters of the shortest word,
-	 and checks whether the respective letters are the same in other words
-		O(NM) where N = Number of strings
-					M = Length of the largest string string """
+"""The algorithm iterates over letters of the shortest word,
+	and checks whether the respective letters are the same in other words
+	O(NM) where N = Number of strings
+				M = Length of the largest string string """
 
-	def findMinimumLength(self, arr, length):
-		minimum = len(arr[0])
+def longestCommonPrefix(arr):
+	length = len(arr)
+	minimum = len(arr[0])
+	minimumLength = [i for i in range(length) if len(arr[i]) < minimum]
+	result = " "
 
-		for i in range(length):
-			if(len(arr[i]) < minimum):
-				minimum = len(arr[i])
-		
-		return minimum
+	for i in range(len(arr[minimumLength[0]])):
+		prefix = arr[0][i]
 
+		for j in range(length):
+			if(arr[j][i] != prefix):
+				return result
 
-	def longestCommonPrefix(self, arr, length):
-		minimumLength = self.findMinimumLength(arr, length)
-		result = " "
-
-		for i in range(minimumLength):
-			prefix = arr[0][i]
-
-			for j in range(length):
-				if(arr[j][i] != prefix):
-					return result
-
-			result += prefix
+		result += prefix
 
 arr = ["flower","flow","flight"]
-length = len(arr)
-solution = Solution()
-result = solution.longestCommonPrefix(arr, length)
+result = longestCommonPrefix(arr)
 print(result)
